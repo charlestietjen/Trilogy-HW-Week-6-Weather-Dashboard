d21674d2640709099cf8961ba34c6e7c451124c9
Charles T <tietjen.charles@gmail.com>
2022-01-02 22:51:49 -0500

    in-session search history completed

--- assets/js/localScript.js
+++ assets/js/localScript.js
@@ -8,6 +8,9 @@ var currentLocName = " ";
 var searchBtnEl = document.querySelector("#search-btn");
 var inputEl = document.querySelector("#city-input");
 var fiveDayEl = document.querySelector("#five-day-weather");
+var searchHistEl = document.querySelector("#prev-cities");
+var searchHist = [];
+var histBtnEl = document.querySelectorAll(".prev-btn");
 
 var geoCode = (function(city){
     var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=5&appid=' + weatherKey;
@@ -18,6 +21,7 @@ var geoCode = (function(city){
                     alert("Entered location returned no results.");
                 } else {
                     currentLocName = data[0].name + ", " + data[0].state;
+                    histBtnHandler(currentLocName);
                     fetchWeather(data[0].lat, data[0].lon);
                 }
             })
@@ -26,6 +30,25 @@ var geoCode = (function(city){
         }
     })
 });
+
+var histBtnHandler = (function(name){
+    for (i = 0; i < searchHist.length; i++) {
+        if (name === searchHist[i]){
+            //do nothing
+            return;
+        }
+    }
+    histBtnEl = document.querySelectorAll(".prev-btn");
+    searchHist.push(name);
+    var newBtn = document.createElement("button");
+    newBtn.classList = "prev-btn button is-fullwidth is-link";
+    newBtn.value = name;
+    newBtn.innerText = name;
+    console.log(newBtn);
+    searchHistEl.appendChild(newBtn);
+    newBtn.addEventListener("click", function() {
+        geoCode(name)});
+})
 
 var fetchWeather = (function(lat, long){
     var apiUrl = 'http://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + long + '&units=metric' + '&appid=' + weatherKey;
@@ -91,49 +114,45 @@ var fillFiveDayWeather = (function(weather) {
         day[i].temp = weather.daily[i].temp.day;
         day[i].wind = weather.daily[i].wind_speed;
         day[i].hum = weather.daily[i].humidity;
-
         var containerCardEl = document.createElement("div");
         containerCardEl.classList = "card";
-
         var uDate = dayjs.unix(weather.daily[i].dt);
-
         var dateEl = document.createElement("h3");
         dateEl.innerText = (uDate.$M + 1) + "/" + uDate.$D + "/" + uDate.$y;
-
         var wIconConEl = document.createElement("div");
         wIconConEl.classList = "card-image";
-
         var wIconEl = document.createElement("figure")
         wIconEl.classList = "image, is-16x16";
         wIconEl.innerHTML = "<img src='" + day[i].icon + "' alt='Weather Icon'>";
-        
         var tempEl = document.createElement("P");
         tempEl.classList = "card-content";
         tempEl.innerText = "Temperature: " + day[i].temp + "C";
-
         var windEl = document.createElement("P");
         windEl.classList = "card-content";
         windEl.innerText = "Wind Speed: " + day[i].wind + "km/h";
-        
         var humEl = document.createElement("P");
         humEl.classList = "card-content";
         humEl.innerText = "Humidity: " + day[i].hum + "%";
-        
-        console.log(fiveDayEl);
-        console.log(containerCardEl);
-
         fiveDayEl.appendChild(containerCardEl);
-        
         containerCardEl.appendChild(dateEl);
         containerCardEl.appendChild(wIconConEl);
         containerCardEl.appendChild(tempEl);
         containerCardEl.appendChild(windEl);
         containerCardEl.appendChild(humEl);
-
         wIconConEl.appendChild(wIconEl);
-
         }
 });
+
+function reloadCss()
+{
+    var links = document.getElementsByTagName("link");
+    for (var cl in links)
+    {
+        var link = links[cl];
+        if (link.rel === "stylesheet")
+            link.href += "";
+    }
+}
 
 searchBtnEl.addEventListener("click", function(){
     var searchTerm = inputEl.value;
